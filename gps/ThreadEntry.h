ff16b9e1bb67d8659486e65d0594d0e47c13299a
youhyeon <nanana126@ajou.ac.kr>
2025-06-01 21:27:39 +0900

    thread entry modify

--- gps/ThreadEntry.h
+++ gps/ThreadEntry.h
@@ -2,6 +2,7 @@
 #define THREADENTRY_H
 
 void* pubEmecyThread(void* arg);
-void * gpsReadThread(void * arg)
+void * gpsReadThread(void * arg);
+void* pubLocationThread(void* arg);
 
 #endif // THREADENTRY_H
